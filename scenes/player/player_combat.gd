5a3f3b6574dda1231272f7972f9d6979898f4010
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2021-11-21 22:25:15 +0000

    Add player_combat scripts

---
+++ scenes/player/player_combat.gd
@@ -0,0 +1,41 @@
+extends KinematicBody2D
+
+onready var fsm = FSM.new(self, $states, $states/idle, true)
+
+var anim_cur = ""
+var anim_nxt = "idle"
+
+enum Position {LEFT=500, CENTER=1250, RIGHT=2000}
+var pos_cur = Position.CENTER
+var dash_dir : String
+
+onready var animations = $animations
+
+
+func _ready():
+#	cur_params = parameters[gamestate.state.current_player_status
+#	game.player = self
+#	var _ret = gamestate.connect( "gamestate_changed", self, "set_attack_reach", [], Object.CONNECT_DEFERRED )
+	pass
+
+
+func _exit_tree():
+	fsm.free()
+
+
+func _physics_process(delta):
+	fsm.run_machine(delta)
+	
+	if anim_cur != anim_nxt:
+		anim_cur = anim_nxt
+		$animations.play(anim_cur)
+
+
+func can_dodge() -> bool:
+	if self.pos_cur == self.Position.LEFT and \
+	   self.dash_dir == "left":
+		return false
+	if self.pos_cur == self.Position.RIGHT and \
+	   self.dash_dir == "right":
+		return false
+	return true
