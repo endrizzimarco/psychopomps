e2b6401d09b4f472ba1a08a992de0cb4830beb1b
Marco Endrizzi <marco.endrizzi@gmail.com>
2021-12-23 02:35:12 +0100

    Reimport images with RAM compression

--- scenes/bosses/boss_1/phase_1/phase_1.gd
+++ scenes/bosses/boss_1/phase_1/phase_1.gd
@@ -4,17 +4,13 @@ var spike_count = 0
 var _timer
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_spike_attack()
+	$atk_anim.play("spikes_atk")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 
 func _spike_attack():
-	_timer = Timer.new()
-	add_child(_timer)
-	_timer.connect("timeout", self, "_on_spike_Timer_timeout")
-	_timer.set_wait_time(1)
-	_timer.set_one_shot(false) # Make sure it loops
-	_timer.start()
+	var spike = preload("res://scenes/bosses/boss_1/phase_1/attacks/spike_atk.tscn").instance()
+	add_child(spike)
 	
 
 func _on_spike_Timer_timeout():
