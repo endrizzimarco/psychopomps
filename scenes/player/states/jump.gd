99adb76d1279e9052df8e6cc8719703cf0a9c897
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2022-01-09 15:32:38 +0000

    Fix jump bounce on wall

--- scenes/player/states/jump.gd
+++ scenes/player/states/jump.gd
@@ -7,13 +7,16 @@ func initialize() -> void:
 func run( _delta ) -> void:	
 	if Input.is_action_just_pressed("btn_right") or \
 		Input.is_action_just_pressed("btn_left"):
+		obj.dash_dir = "right" if Input.is_action_just_pressed("btn_right") else "left"
 		var pos_cur = obj.pos_cur
 		var pos_nxt
 		if Input.is_action_just_pressed("btn_right"):
 			pos_nxt = obj.Position.RIGHT if obj.pos_cur == obj.Position.CENTER else obj.Position.CENTER
 		else: 
 			pos_nxt = obj.Position.LEFT if obj.pos_cur == obj.Position.CENTER else obj.Position.CENTER
-		_airdash(pos_cur, pos_nxt)
+		
+		if obj.can_dodge():
+			_airdash(pos_cur, pos_nxt)
 	
 	# If no animations are playing, player has finished jumping and can go back to idle
 	if not $'../../animations'.current_animation:
