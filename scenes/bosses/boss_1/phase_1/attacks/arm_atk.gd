9638c39749a12b8c3841b155a4556497bcb79be3
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2022-01-09 00:10:51 +0000

    Arm attack now recognizes correct side

--- scenes/bosses/boss_1/phase_1/attacks/arm_atk.gd
+++ scenes/bosses/boss_1/phase_1/attacks/arm_atk.gd
@@ -1,17 +1,21 @@
 extends Node2D
 
-
 var rng = RandomNumberGenerator.new()
+var side = ''
 
-# Play a random arm_atk
+# https://godotengine.org/qa/4786/how-to-pass-parameters-when-instantiating-a-node
+func init(curr_side):
+	side = curr_side 
+
+
+# Play a random arm_atk based on which side the boss currently is
 func _ready():
 	rng.randomize()
-	var randint = rng.randi_range(0, 3)
-	$anim.play(str(randint))
+	var anim_name = side + '_' + str(rng.randi_range(0, 1))
+	$anim.play(anim_name)
 
 
 # Remove scene after animation plays
 func _process(_delta):
 	if not $anim.current_animation:
 		queue_free()
-
--- scenes/bosses/boss_1/phase_1/phase_1.gd
+++ scenes/bosses/boss_1/phase_1/phase_1.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var curr_side = "left"
+var curr_side = "right"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$atk_master.play("pattern_1")
@@ -14,6 +14,7 @@ func _spike_attack():
 
 func _arm_attack():
 	var arm_atk = preload("res://scenes/bosses/boss_1/phase_1/attacks/arm_atk.tscn").instance()
+	arm_atk.init(curr_side)
 	add_child(arm_atk)
 
 
@@ -22,9 +23,9 @@ func _tongue_attack():
 	add_child(tongue_atk)
 
 func _change_side():
-	if curr_side == "left":
-		curr_side = "right"
+	if curr_side == "right":
+		curr_side = "left"
 		$atk_anim.play("change_side")
 	else:
-		curr_side = "left"
+		curr_side = "right"
 		$atk_anim.play_backwards("change_side")
