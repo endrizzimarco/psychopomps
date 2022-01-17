f490bd0783e43c72b214ccf2510f78c1c124666a
SomeToffee <taiprult@gmail.com>
2022-01-17 18:12:37 +0000

    door fixed

--- scenes/Fake RPG intro/Door.gd
+++ scenes/Fake RPG intro/Door.gd
@@ -1,4 +1,4 @@
 extends Area2D
 
-#func _on_Door_body_entered(_body):
-#	get_tree().change_scene("res://scenes/Fake RPG intro/player/player.tscn")
+func _on_Door_body_entered(_body):
+	get_tree().change_scene("res://scenes/Fake RPG intro/player/player.tscn")
